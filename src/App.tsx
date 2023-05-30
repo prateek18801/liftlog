65d170cfc3ecd41757bc348747bcb7733ae1184f
Prateek Chaurasia <prateek18801@gmail.com>
2023-05-30 16:52:06 +0530

    Add page number state manage pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import Home from './pages/Home';
+import Stats from './pages/Stats';
 
 const App = () => {
+    const [page, setPage] = useState<number>(0);
     return (
         <>
-            <Home />
+            {page ? <Stats setPage={setPage} /> : <Home setPage={setPage} />}
         </>
     );
 }
